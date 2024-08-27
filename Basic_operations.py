028dd4805168e659cfa83cbd88f8c4566a2494f9
Skempf1442 <skempf@students.mchenry.edu>
2024-08-26 21:02:35 -0500

    Add files via upload

---
+++ Basic_operations.py
@@ -0,0 +1,41 @@
+
+# Addition
+sum = 51 + 12
+print("\nAddition: ")
+print(sum)
+
+# Subtraction
+diff = 95 - 34
+print("\nSubtraction: ")
+print(diff)
+
+# Multiplication
+product = 2 * 10
+print("\nMultiplication: ")
+print(product)
+
+# Division
+quotient = 120 / 4
+print("\nDivision: ")
+print(quotient)
+
+# Integer Division
+integer_div = 22 // 2
+print("\nInteger Division: ")
+print(integer_div)
+
+# Modulus
+remainder = 100 % 5
+print("\nModulus: ")
+print(remainder)
+
+# Exponentiation
+exp = 10 ** 5
+print("\nExponentiation:")
+print(exp)
+
+month = "April"
+day = 7
+year = 2006
+
+print("Your birthday is: " + month + " " + str(day) + ", " + str(year))
---
+++ Basic_operations_pt2.py
@@ -0,0 +1,9 @@
+# Given length and area
+length = 20
+area = 15
+
+# Calculate width using the formula: area = length * width
+width = area + length
+
+# Print the result
+print(f"The area of the rectangle is {area}, with length {length} and width {width}.")
