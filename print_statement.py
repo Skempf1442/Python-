20fcee2c27d8ca4a57cbdd895eb8a771c9f2cec7
Skempf1442 <skempf@students.mchenry.edu>
2024-08-26 20:19:37 -0500

    Add files via upload

---
+++ print_statement.py
@@ -0,0 +1,12 @@
+# Knock-Knock Joke Program
+print("Knock, knock.")  
+print("Who's there?")  
+print("Your Mom")  
+print("Your mom who?")  
+print("Your mom says hey")  
+
+print()
+# As one line of code using escape characters
+print("Knock, knock.\nWho's there?\nYourmom.\nYourmomwho?\nYourmomsayshey")
+
+#this statement is useful and the most important for debugging and also make your coding life easier as you can write one line of code with the n/ function and all the other functions 
