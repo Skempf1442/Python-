5a6f2ca878d8c75b8e86a1b6c904035e390c5ca6
Skempf1442 <skempf@students.mchenry.edu>
2024-08-26 20:39:17 -0500

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,26 @@
+age=18 
+name="Abi"
+
+print ("My age is",age)
+print ("My name is",name)
+
+
+# Literal
+x = 1442
+print("Literal:", x)
+
+# Integer
+y = -1000
+print("Integer:", y)
+
+# Float
+z = 20
+print("Float:", z)
+
+# String
+name = "Abi"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
