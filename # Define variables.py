9e76d3a3c4eeeaec5ec565ba29b45845c4ab71d4
Skempf1442 <skempf@students.mchenry.edu>
2024-09-03 08:55:01 -0500

    Add files via upload

---
+++ # Define variables.py
@@ -0,0 +1,11 @@
+#Define variables for kilogram value 
+kg_value_1= 5 
+
+#Conversion factor: 1 pound = 2.20462 kilograms 
+conversion_factor = 2.20462 
+
+#Calculate pounds for each kilogram value 
+pound_1 = kg_value_1 * conversion_factor
+
+#Output the results 
+print(f"{kg_value_1} kilogram is equal to {pound_1: 2f} pounds." ) 
