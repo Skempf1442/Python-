12b026c99352f7c83d870a57e097adc85212a75f
Skempf1442 <skempf@students.mchenry.edu>
2024-09-03 09:01:27 -0500

    Add files via upload

---
+++ PE1 2.5.py
@@ -0,0 +1,8 @@
+#finding mpg for a car 
+tank_size = 15
+distance = 600
+#variables for equation 
+mpg = distance / tank_size
+#equation for finding mpg 
+print("Miles per gallon:", mpg)
+#display this message on the screen 
