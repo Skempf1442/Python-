53bed3f843f2831e993ff85ef7a4e823ad5f4c87
Skempf1442 <skempf@students.mchenry.edu>
2024-09-09 21:51:39 -0500

    Add files via upload

---
+++ 3.1 A.py
@@ -0,0 +1,33 @@
+# age = int(input("How old are you?"))
+#define the age
+age = 22 
+
+#check age for age realetd content 
+print(f"How old are you? {age}")
+
+if age >= 16:
+    print("You are old enough")
+else:
+    print("You are not old enough to drive")
+
+#can you vote 
+
+if age >=18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+
+#can you buy alcohol legally 
+
+if age >=21:
+    print("You can legally.")
+else:
+    print("You cannot buy legally")
+
+#senior discount
+
+if age >=65:
+    print("You are eligible for senior discount")
+else:
+    print("You are not old enough for senior discount")
+    
