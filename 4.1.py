af67f184b2553c864e5479989b5ee3ca6a57ea8b
Skempf1442 <skempf@students.mchenry.edu>
2024-09-18 12:18:47 -0500

    Add files via upload

---
+++ 4.1.py
@@ -0,0 +1,13 @@
+# def the square function 
+def square (side):
+    area= side*side 
+    print("The area of the sqaure is " + str(area) + "square units")
+square(5)
+
+#def the circle function 
+def circle (radius):
+    pi = 3.14 
+    area = pi * radius * radius 
+    print("The area of the circle is" + str(area) + "square units.")
+circle (4)
+    
